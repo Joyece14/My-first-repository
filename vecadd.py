246a4de148205ca47fa21421231a0f13a24d4f81
prakash <2024ucp1800@mnit.ac.in>
2025-11-17 15:19:10 +0530

    this commint on freinds repo

--- vecadd.py
+++ vecadd.py
@@ -12,7 +12,7 @@ vector2 = list(map(int, input("elements of vector 2 separated by space: ").split
 >>>>>>> parallel
 
 if len(vector1) != len(vector2):
-    print("Vectors must have the same length")
+    print("Vectors must have the same")
 else:
 
     dot_product = sum(a * b for a, b in zip(vector1, vector2))
